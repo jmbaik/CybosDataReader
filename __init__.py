5f80442e31161b74ca9a8021f3e9f9c233efc9d8
jmbaik <famian@daum.net>
2022-09-04 00:13:59 +0900

    working day considering

--- __init__.py
+++ __init__.py
@@ -1,9 +1,17 @@
 from reader import cybos_reader_codex
 from krx import mig_krx
+import sys
+from datetime import datetime
+from pykrx import stock
 
 
 class Main:
     def __init__(self):
+        _ymd = datetime.today().strftime('%Y%m%d')
+        _workday = stock.get_nearest_business_day_in_a_week(_ymd)
+        if _ymd > _workday:
+            print('today is not stock working day')
+            sys.exit()
         cybos_reader = cybos_reader_codex.CybosDataReaderCodex()
         if cybos_reader.connected() is True:
             cybos_reader.update_price_m1(tick_count=1000)
